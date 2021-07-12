713e49e74b32b7fb46df3e89cdfbcbe56afcc989
Karensza <k25gonz@gmail.com>
2021-07-11 22:54:53 -0500

    algunos delay, cambio al menú opciones canal, primera parte suscripción a canal

--- src/main/kotlin/models/Channel.kt
+++ src/main/kotlin/models/Channel.kt
@@ -5,10 +5,12 @@ data class Channel(val name:String,val user: User,val description:String){
 
     init {
         user.haveAchannel=true
+        user.upload = true
     }
 
     val videos = mutableListOf<Video>()
     val subscribers= mutableListOf<User>()
+
 
 }
 
